39ee2b4d9cdbf73d59c64f37f84a365d02d6a2fe
Snir Vitrak Tamam <snirv@migal.org.il>
2020-02-05 13:30:27 +0200

    implements the api for evolution

---
+++ ga.py
@@ -0,0 +1,46 @@
+
+import numpy as np
+import pandas as pd
+
+
+class GA:
+    def __init__(self):
+        pass
+
+    def create_population(self):
+        pass
+    
+    def natural_selection(self): # fitness -> mating_pool -> create_new_generation -> mutate -> crossover
+        pass
+
+    def fitness(self):
+        pass
+
+    def mating_pool(self):
+        pass
+
+    def create_new_generation(self):
+        pass
+
+    def mutate_population(self):
+        pass
+
+    def crossover_population(self):
+        pass
+    
+    def translate_genotype(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+

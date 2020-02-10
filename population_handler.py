e562f369c8f9a80956b5460e213ed097ec53d7b9
Snir Vitrak Tamam <snirv@migal.org.il>
2020-02-10 19:43:38 +0200

    added mutation method + refactoring

--- population_handler.py
+++ population_handler.py
@@ -2,16 +2,17 @@ import numpy as np
 
 ###################################
 from individual import Individual
+from mutation_handler import Mutation_Handler
 ###################################
 
 """This class is responsible for creating the population and applying actions upon the individuals"""
 class Population_Handler():
-    def __init__(self, num_features, probability_handler, max_individual_size = 10):
-        self.num_features = num_features
+    def __init__(self, probability_handler, max_individual_size = 10):
         self.probability_handler = probability_handler
         self.max_individual_size = max_individual_size
         self.population = []
-        self.individual = Individual(self.num_features, self.max_individual_size)
+        self.individual = Individual(self.probability_handler.max_feature_number, self.max_individual_size)
+        self.mh = Mutation_Handler(self.probability_handler)
         
     def create_individual(self):
         return self.individual.be_born()
@@ -21,10 +22,12 @@ class Population_Handler():
         for i in range(population_size):
             self.population[i]=self.create_individual()
 
-    def mutate(self):
+    def mutate_population(self):
+        for individual in self.population:
+            self.mh.mutate(individual)
+
+    def cross_over_population(self):
         pass
-    def cross_over(self):
-        pass
 
-
-
+    def get_population(self):
+        return self.population
--- tests/test_population_handler.py
+++ tests/test_population_handler.py
@@ -12,22 +12,31 @@ from probability_handler import Probability_Handler
 class Test_Population_Handler(unittest.TestCase):
 
     def test_create_individual(self):
-        probability_handler = Probability_Handler()
-        ph = Population_Handler(num_features = 10, probability_handler=probability_handler, max_individual_size = 10)
+        ph = create_test_population_handler()
         ind = ph.create_individual()
         self.assertIsNotNone(ind)
 
     def test_create_population(self):
-        max_individual_size = 10
-        num_features = 10
         population_size = 10
-        probability_handler = Probability_Handler()
-
-        ph = Population_Handler(num_features = num_features, probability_handler=probability_handler, max_individual_size = max_individual_size)
+        ph = create_test_population_handler()
         ph.create_population(population_size = population_size)
         self.assertIsNotNone(ph.population)
         self.assertEqual(len(ph.population), population_size)
-    
+
+
+    def test_mutate_population(self):
+        ph = create_test_population_handler()
+        population_size = 10
+        ph.create_population(population_size = population_size)
+        ph.mutate_population()
+        self.assertIsNotNone(ph.population)
+        self.assertEqual(len(ph.population), population_size)
+
+
+def create_test_population_handler(max_individual_size = 10, num_features = 10):
+    probability_handler = Probability_Handler(max_feature_number = 10)
+    ph = Population_Handler(probability_handler=probability_handler, max_individual_size = max_individual_size)
+    return ph
 
 
 if __name__ == '__main__':

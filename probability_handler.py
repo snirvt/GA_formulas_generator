2756cf77e1d693e1c9e3668f9583e5cd2496987c
Snir Vitrak Tamam <snirv@migal.org.il>
2020-02-10 19:44:21 +0200

    added a uniform probability

--- probability_handler.py
+++ probability_handler.py
@@ -4,18 +4,23 @@ import constants
 
 class Probability_Handler():
 
-    def __init__(self):
+    def __init__(self, max_feature_number):
         self.prob_dict = {}
-        self.prob_dict[constants.DNA_SIZE_STR] = 0.2
-        self.prob_dict[constants.DNA_FEATURES_STR] = 0.2
-        self.prob_dict[constants.DNA_WEIGHTS_STR] = 0.2
-        self.prob_dict[constants.DNA_PARENTHESES_STR] = 0.2
-        self.prob_dict[constants.DNA_ACTIONS_STR] = 0.2
+        self.max_feature_number = max_feature_number
+
+    def create_uniform_probability(self, chance = 0.2):
+        self.prob_dict[constants.DNA_SIZE_STR] = chance
+        self.prob_dict[constants.DNA_FEATURES_STR] = chance
+        self.prob_dict[constants.DNA_WEIGHTS_STR] = chance
+        self.prob_dict[constants.DNA_PARENTHESES_STR] = chance
+        self.prob_dict[constants.DNA_ACTIONS_STR] = chance
 
     def set_ptobability(self, prob_dict):
         self.prob_dict = prob_dict
 
     def get_probability_dict(self):
+        if len(self.prob_dict) == 0:
+            self.create_uniform_probability()
         return self.prob_dict
 
 

cef9a0f08d6895eb1f1e31b45e3feba282476ec5
Snir Vitrak Tamam <snirv@migal.org.il>
2020-02-19 13:02:54 +0200

    added action switch, and r2 score

--- utils.py
+++ utils.py
@@ -9,3 +9,17 @@ def activation():  # sin cos exp...
 def swap_binary_value(binary_val):
     return 1 - binary_val
 
+def r2_score(y_true, y_pred):
+    SS_reg = np.sum((y_pred - y_true)**2)
+    SS_tot = np.sum((np.mean(y_true) - y_true)**2)
+    return 1 - SS_reg/SS_tot
+
+def get_action(action):
+    switcher = {
+        0:'+',
+        1:'-',
+        2:'*',
+        3:'/',
+        4:'**',
+    }
+    return switcher.get(int(action))

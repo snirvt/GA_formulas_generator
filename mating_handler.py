474be677ea7c3f205c145ea6e5a28fd6e4980de2
Snir Vitrak Tamam <snirv@migal.org.il>
2020-02-19 13:05:02 +0200

    class for selecting parents for mating

---
+++ mating_handler.py
@@ -0,0 +1,19 @@
+
+import numpy as np
+import random
+
+class Mating_Handler():
+
+    def __init__(self, num_offsprings_per_parent):
+        self.num_offsprings_per_parent = num_offsprings_per_parent
+    
+    def choose_parents(self, population, population_fitness):
+        sorted_population = self.sort_by_fitness(population, population_fitness)
+        num_parents = int( len(population) / self.num_offsprings_per_parent )
+        sampled_population = random.choices(sorted_population[int(num_parents/2):], k = int(num_parents/2))
+        sorted_population[int(num_parents/2):num_parents] = sampled_population ## keep the best num_parents/2 parents, the rest are random
+        return sorted_population, num_parents
+
+    def sort_by_fitness(self, population, population_fitness):
+        fitness_sorting_index = np.argsort(population_fitness)
+        return population[fitness_sorting_index[::-1]] # reverse sort population by fitness
---
+++ tests/test_mating_handler.py
@@ -0,0 +1,31 @@
+
+import numpy as np
+import pytest
+import mock
+
+import unittest   # The test framework'
+from unittest.mock import MagicMock
+
+###################################
+from mating_handler import Mating_Handler
+
+
+@pytest.fixture(name="mate_handler")
+def mating_fixture(mocker):
+    #  Bring up
+    yield Mating_Handler(num_offsprings_per_parent = 2)
+    #  Tear Down
+
+@pytest.mark.parametrize('population', [np.array(['e','c','b','a','d','f'])])
+@pytest.mark.parametrize('fittness', [np.array([5,3,2,1,4,6])])
+def test_sort_by_fitness(population, fittness, mate_handler):
+    reversed_sorted = mate_handler.sort_by_fitness(population = population, population_fitness = fittness)
+    assert(np.all(reversed_sorted == np.array(['f', 'e', 'd', 'c', 'b', 'a'])))
+
+@pytest.mark.parametrize('population', [np.array(['e','c','b','a','h','d','g','f'])])
+@pytest.mark.parametrize('fittness', [np.array([5,3,2,1,8,4,7,6])])
+def test_choose_parents(population, fittness, mate_handler):
+    sorted_population, num_parents = mate_handler.choose_parents(population = population, population_fitness = fittness)
+    assert(num_parents == 4)
+    assert(np.all(sorted_population[:2] == np.array(['h', 'g']))) ## check for num_parents/2 best individuals are parents
+

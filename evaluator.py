26199cfbbb652e1b671106d4e47151ffaf7ea8d4
Snir Vitrak Tamam <snirv@migal.org.il>
2020-02-19 12:59:44 +0200

    now can evaluate incoming population

--- evaluator.py
+++ evaluator.py
@@ -4,6 +4,7 @@ import numpy as np
 import constants
 from individual import Individual
 from tree import Tree
+import utils
 
 class Evaluator():
 
@@ -11,6 +12,8 @@ class Evaluator():
         self.X = X
         self.y = y
         self.column_names = column_names
+        self.tree = Tree()
+        self.expression_str = ''
 
     def evaluate_population(self, population):
         fitness_array = np.empty([len(population)])
@@ -19,21 +22,54 @@ class Evaluator():
         return fitness_array
 
     def evaluate_individual(self, individual):
-        tree = self.create_individual_binary_tree(individual)
-        return 0
+        self.build_individual_binary_tree(individual)
+        self.expression_str = ''
+        self.extract_tree_expression(self.tree.node, index_mark = '_')
+        self.fix_expression()
+        X = self.X ## for short string on eval
+        return utils.r2_score(self.y, eval(self.build_math_evaluation(self.expression_str)))
 
-    def create_individual_binary_tree(self, individual):
+    def build_individual_binary_tree(self, individual):
         merged_values = Individual.merge_dna_data(individual)
-        tree = Tree()
+        self.tree.delete_tree()
         for value in merged_values:
-            tree.insert(value)
-        return tree
+            self.tree.insert(value)
+
+    def extract_tree_expression(self, node, index_mark = '_'):
+        if node == None or node.data == None:
+            return self.expression_str
+        feature, parentheses, action = Individual.get_all_merged_values(node.data)
+        
+        if parentheses == 1:
+            self.expression_str += '('
+
+        self.expression_str += '{}{}{}'.format(index_mark, feature, index_mark)
+        self.expression_str += utils.get_action(action)
+
+        self.expression_str = self.extract_tree_expression(node.left, index_mark)
+        self.expression_str = self.extract_tree_expression(node.right, index_mark)
+
+        if parentheses == 1:
+            self.expression_str = self.expression_str[:-1] + ')' + self.expression_str[-1] ## put closing parentesis before action
+        return self.expression_str
+
+    def fix_expression(self):
+        self.expression_str = self.expression_str[:-1] ## remove the last action
+
+    
+    def build_math_evaluation(self, expression):
+        split_expression = expression.split('_')
+        math_str = ''
+        for split in split_expression:
+            if split.isnumeric():
+                math_str += self.get_string_data_column(int(split))
+            else:
+                math_str += split
+        return math_str
 
 
-
-
-
-
+    def get_string_data_column(self, column_number):
+        return 'X[:,{}]'.format(column_number)
 
 
 
--- tests/test_evaluator.py
+++ tests/test_evaluator.py
@@ -9,32 +9,72 @@ from unittest.mock import MagicMock
 
 ###################################
 from evaluator import Evaluator
+from tests.test_common import create_data, create_simple_individual
 ###################################
 
 class Test_Evaluator(unittest.TestCase):
 
-    def test_create_population(self):
+    def test_build_individual_binary_tree(self):
         dna_dict,pre_order_correct = create_dna_dict()
-        evaluator = Evaluator(X=None,y=None, column_names=None)
-        tree = evaluator.create_individual_binary_tree(dna_dict)
+        evaluator = Evaluator(X=None, y=None, column_names=None)
+        evaluator.build_individual_binary_tree(dna_dict)
+        tree = evaluator.tree
         self.assertIsNotNone(tree)
-
         pre_order_result = []
         tree.pre_order(pre_order_result)
         self.assertIsNotNone(pre_order_result)
         self.assertEqual(pre_order_result, pre_order_correct)
+        evaluator.build_individual_binary_tree(dna_dict) ## check if the first tree is deleted
+        self.assertEqual(pre_order_result, pre_order_correct)
 
 
-
-
+    def test_extract_tree_expression(self):
+        dna_dict, _ = create_dna_dict()
+        evaluator = Evaluator(X=None, y=None, column_names=None)
+        evaluator.build_individual_binary_tree(dna_dict)
+        tree = evaluator.tree
+        expression = evaluator.extract_tree_expression(tree.node, index_mark = '_')
+        self.assertIsNotNone(expression)
+        self.assertEqual(expression, '(_1_+_3_*_5_**(_2_-(_4_)/_6_))+')
+        
 
 def create_dna_dict():
     dna_dict = {}
     dna_dict['size'] = 6
-    dna_dict['feature_indices'] = [0,1,2,3,4,5]
+    dna_dict['feature_indices'] = [1,2,3,4,5,6]
     dna_dict['weights'] = [3,5,1,4,2,6]
     dna_dict['parentheses_binary_vec'] = [1,1,0,1,0,0]
     dna_dict['actions'] = [0,1,2,3,4,0]
 
-    pre_order_correct = [(3, 0, 1, 0), (1, 2, 0, 2), (2, 4, 0, 4), (5, 1, 1, 1), (4, 3, 1, 3), (6, 5, 0, 0)]
+    pre_order_correct = [(3, 1, 1, 0), (1, 3, 0, 2), (2, 5, 0, 4), (5, 2, 1, 1), (4, 4, 1, 3), (6, 6, 0, 0)]
     return dna_dict, pre_order_correct
+
+
+
+@pytest.fixture(name="eval_handler")
+def evaluator_fixture(mocker):
+    #  Bring up
+    X, y , column_names = create_data()
+    yield Evaluator(X=X, y=y, column_names=column_names)
+    #  Tear Down
+
+@pytest.mark.parametrize('input_num', [-1, 0, 1, 10])
+def test_get_string_data_column(input_num, eval_handler):
+    output = eval_handler.get_string_data_column(input_num)
+    assert(output == 'X[:,{}]'.format(input_num))
+
+
+@pytest.mark.parametrize('input_str', ['(_1_+_3_*_5_**(_2_-(_4_)/_6_))'])
+def test_build_math_evaluation(input_str, eval_handler):
+    output = eval_handler.build_math_evaluation(input_str)
+    assert(output == '(X[:,1]+X[:,3]*X[:,5]**(X[:,2]-(X[:,4])/X[:,6]))')
+
+
+def test_evaluate_individual(eval_handler):
+    individual = create_simple_individual()
+    output = eval_handler.evaluate_individual(individual)
+    assert(output == 1)
+
+
+
+

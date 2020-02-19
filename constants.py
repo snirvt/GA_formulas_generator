d64433030cfae8faaf0330db7ffdefbaa9520d4e
Snir Vitrak Tamam <snirv@migal.org.il>
2020-02-19 12:58:39 +0200

    small change

--- constants.py
+++ constants.py
@@ -3,7 +3,8 @@
 ##### numerics #####
 MAX_SIZE_INDIVIDUAL = 10
 MIN_SIZE_INDIVIDUAL = 1
-NUM_ACTIONS = 6
+NUM_ACTIONS = 5
+
 
 #### dna dictionary keys ####
 DNA_SIZE_STR = 'size'
@@ -11,3 +12,12 @@ DNA_WEIGHTS_STR = 'weights'
 DNA_FEATURES_STR = 'feature_indices'
 DNA_PARENTHESES_STR = 'parentheses_binary_vec'
 DNA_ACTIONS_STR = 'actions'
+
+
+
+### changable stats
+NUM_OFFSPRINGS_PER_PARENT = 5
+
+
+
+

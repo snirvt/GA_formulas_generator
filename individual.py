99c4fa0837d23fe6ec4e6d8494aac847c15d21ec
Snir Vitrak Tamam <snirv@migal.org.il>
2020-02-25 11:41:53 +0200

    refactor

--- individual.py
+++ individual.py
@@ -3,6 +3,7 @@ import random
 ###################################
 import numbers_generator as ng
 import constants
+import features_generator as fg
 ###################################
 
 class Individual():
@@ -12,12 +13,13 @@ class Individual():
         self.num_actions = num_actions
         
     def be_born(self):
-        self.individual_size = ng.generate_gaussian_integers(mu=3, sigma=1, lower_limit=1, upper_limit=5)[0]
-        self.feature_indices = ng.generate_n_uniform_random_integers(min_val=0, max_val = self.num_features, size = self.individual_size)
-        self.weights = ng.generate_n_uniform_random_values(self.individual_size)
-        self.parentheses_binary_vec = ng.generate_n_binary_numbers(size=self.individual_size)
-        self.actions = ng.generate_n_uniform_random_integers(min_val=0 , max_val = self.num_actions , size = self.individual_size)
-        self.wl_scalars = ng.generate_gaussian(mu=1, sigma = 0.5, size = self.individual_size)
+        self.individual_size = fg.create_size_feature()
+        self.feature_indices = fg.create_feature_index_vector(self.num_features, self.individual_size)
+        self.weights = fg.create_weight_vector(self.individual_size)
+        self.parentheses_binary_vec = fg.create_parentheses_vector(size = self.individual_size )
+        self.actions = fg.create_action_vector(size = self.individual_size)
+        self.wl_scalars = fg.create_wl_scalars_vector(mu=1, sigma = 0.5, size = self.individual_size)
+        self.wl_powers = fg.create_wl_scalars_vector(mu=1, sigma = 0.5, size = self.individual_size)
         return self.create_dna_dictionary()
 
     def create_dna_dictionary(self):
@@ -28,7 +30,7 @@ class Individual():
         self.dna_dict[constants.DNA_PARENTHESES_STR] = self.parentheses_binary_vec
         self.dna_dict[constants.DNA_ACTIONS_STR] = self.actions
         self.dna_dict[constants.DNA_WL_SCALAR] = self.wl_scalars
-
+        self.dna_dict[constants.DNA_WL_POWER] = self.wl_powers
         return self.dna_dict
     
     @staticmethod
@@ -38,7 +40,8 @@ class Individual():
         parentheses_values = dna_dict[constants.DNA_PARENTHESES_STR]
         actions_values = dna_dict[constants.DNA_ACTIONS_STR]
         wl_scalars = dna_dict[constants.DNA_WL_SCALAR]
-        return list(zip(tree_values, feature_values, parentheses_values, actions_values, wl_scalars))
+        wl_powers = dna_dict[constants.DNA_WL_POWER]
+        return list(zip(tree_values, feature_values, parentheses_values, actions_values, wl_scalars, wl_powers))
     
     @staticmethod
     def get_merged_feature_values(merged):
@@ -57,8 +60,12 @@ class Individual():
         return merged[4]
 
     @staticmethod
+    def get_merged_wl_powers(merged):
+        return merged[5]
+
+    @staticmethod
     def get_all_merged_values(merged):
-        return merged[1], merged[2] ,merged[3], merged[4]
+        return merged[1], merged[2] ,merged[3], merged[4], merged[5]
 
         
 

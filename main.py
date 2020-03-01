6a77c68e6a3feab345d7f88326c9222cb33654ba
Snir Vitrak Tamam <snirv@migal.org.il>
2020-03-01 14:40:04 +0200

    added test for ga

--- main.py
+++ main.py
@@ -7,16 +7,14 @@ from ga import GA
 from data_parser import Data_Parser
 
 
-X = np.random.rand(1000,5)
-y = X[:,0]**(X[:,1]  + 2*X[:,2])
+X = np.random.rand(2000,5)
+y = X[:,0]**2 + X[:,1]**2
 column_names = [0,1,2,3,4]
 
 # path = 'data.xlsx'
 
-
-
 # X, y, column_names = Data_Parser.get_matrix_data(path)
 if __name__ == '__main__':    
-    ga = GA(X=X, y=y, column_names = column_names, parlal=False)
+    ga = GA(X=X[:1000,:], X_test=X[1000:,:], y=y[:1000], y_test= y[1000:], column_names = column_names, parlal=False)
     ga.natural_selection()
     # ga.natural_selection_paralal()

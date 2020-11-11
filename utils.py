a6ef503bde04628dc96eeae0e5f0f13e5eea5436
Snir Vitrak Tamam <snirv@migal.org.il>
2020-11-11 15:05:13 +0200

    added snv function

--- utils.py
+++ utils.py
@@ -33,7 +33,13 @@ def get_activation(activation):  # sin cos exp...
 
 
 
-
+def snv(input_data):
+    # Define a new array and populate it with the corrected data  
+    data_snv = np.zeros_like(input_data)
+    for i in range(input_data.shape[0]):
+        # Apply correction
+        data_snv[i,:] = (input_data[i,:] - np.mean(input_data[i,:])) / np.std(input_data[i,:])
+    return data_snv
 
 
 
